a0178267deea6ccfe9071c338d9bc6e84c751ec6
Brandon Chung <chung.brandon123@gmail.com>
2024-03-18 00:00:10 -0500

    Created POST login route

--- api/login/index.go
+++ api/login/index.go
@@ -2,11 +2,13 @@ package login
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/Team-We-are-Cooking/fueltility-backend/schema"
 	fueltilityhttp "github.com/Team-We-are-Cooking/fueltility-backend/wrappers/http"
 	fueltilitysupabase "github.com/Team-We-are-Cooking/fueltility-backend/wrappers/supabase"
+	"golang.org/x/crypto/bcrypt"
 )
 
 
@@ -16,35 +18,54 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	method := r.Method
 
-	_, err := fueltilitysupabase.CreateClient()
+	client, err := fueltilitysupabase.CreateClient()
 	if err != nil {
+		log.Println(err.Error())
 		crw.SendJSONResponse(http.StatusInternalServerError, fueltilityhttp.ErrorResponse{
 			Success: false,
 			Error: &fueltilityhttp.ErrorDetails{Message: "Unable to connect to database."},
 		})
+
+		return
 	}
 
 	switch method {
-	case "GET":
-		var data []schema.User = []schema.User{
-			{Username: "bv", Password: "", Email: "email", FirstName: "Brandon", LastName: "C", Address: "address", City: "city", State: "TX", ZipCode: "134555"},
-		}
-
-		crw.SendJSONResponse(http.StatusOK, fueltilityhttp.Response[schema.User]{
-			Success: true,
-			Data: data,
-		})
 	case "POST":
 		var userCreds schema.Credentials
 
 		if err := json.NewDecoder(r.Body).Decode(&userCreds); err != nil {
+			log.Println(err.Error())
 			crw.SendJSONResponse(http.StatusInternalServerError, fueltilityhttp.ErrorResponse{
 				Success: false,
 				Error: &fueltilityhttp.ErrorDetails{Message: "Internal server error."},
 			})
+
+			return
+		}
+		
+		var foundUser schema.Credentials
+		if _, err := client.From("User").Select("*", "", false).Eq("username", userCreds.Username).Single().ExecuteTo(&foundUser); err != nil {
+			log.Println(err.Error())
+			crw.SendJSONResponse(http.StatusUnauthorized, fueltilityhttp.ErrorResponse{
+				Success: false,
+				Error: &fueltilityhttp.ErrorDetails{Message: "Invalid username or password."},
+			})
+
+			return
 		}
 
-		var data []schema.Credentials
+		if err := bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(userCreds.Password)); err != nil {
+			log.Println(err.Error())
+			crw.SendJSONResponse(http.StatusUnauthorized, fueltilityhttp.ErrorResponse{
+				Success: false,
+				Error: &fueltilityhttp.ErrorDetails{Message: "Invalid username or password."},
+			})
+
+			return
+		}
+
+		var data []schema.Credentials = make([]schema.Credentials, 1)
+		data[0] = schema.Credentials{Username: foundUser.Username,  Email: foundUser.Email}
 		
 		crw.SendJSONResponse(http.StatusOK, fueltilityhttp.Response[schema.Credentials]{
 			Success: true,
